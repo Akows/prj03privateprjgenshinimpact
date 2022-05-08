1b973c7a02614572488b9771b84da8443ec16aae
LeeYuSeung <akows141@gmail.com>
2022-05-08 18:08:52 +0900

    [리팩토링] 프로젝트 코드 안정화
    - 리액트 18버전와 충돌하여 작동 안정성을 심대하게 해치는 마테리얼-UI를 제거.
    - 향후 UI는 HTML/CSS로 손수 작성하거나 React-BootStrap을 사용할 예정.

--- projectfrontend/src/App.js
+++ projectfrontend/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Appbar from './components/Appbar';
-import Mainpage from './components/Mainpage';
+import Mainpage from './pages/Mainpage';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
       <Appbar/>
-      <Mainpage>
-        <h1>메인 화면입니다.</h1>
-        <p>홈페이지의 가장 첫 화면입니다.</p>
-      </Mainpage>
+      <Mainpage/>
     </div>
   );
 }
---
+++ projectfrontend/src/components/Charactersinfo.js
@@ -0,0 +1,13 @@
+import "../Styles/Charactersinfo.css";
+
+const Charactersinfo = () => {
+    return (
+        <div id="charactersinfo">
+            <h1>캐릭터 소개 화면입니다.</h1>
+            <p>캐릭터 소개 화면입니다.</p>
+            <p>캐릭터 소개 화면입니다.</p>
+        </div>
+    );
+}
+
+export default Charactersinfo;
