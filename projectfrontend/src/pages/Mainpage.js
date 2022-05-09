f06d4741a4dfda7be5a309055a11c9ca995729ef
LeeYuSeung <akows141@gmail.com>
2022-05-09 18:31:07 +0900

    [메인 페이지]
    - 반응형 웹페이지 기능 테스트를 위해 배경 이미지를 추가하여 페이지 세로 길이를 늘림.

--- projectfrontend/src/pages/Mainpage.js
+++ projectfrontend/src/pages/Mainpage.js
@@ -2,10 +2,22 @@ import "../styles/Mainpage.css";
 
 const Mainpage = () => {
     return (
-        <div id="mainpage">
-            <h1>메인 화면입니다.</h1>
-            <p>홈페이지의 가장 첫 화면입니다.</p>
-            <p>홈페이지의 가장 첫 화면입니다.</p>
+        <div>
+            <div className="mainpage1">
+
+            </div>
+
+            <div className="mainpage2">
+
+            </div>
+
+            <div className="mainpage3">
+
+            </div>
+
+            <div className="mainpage4">
+
+            </div>
         </div>
     );
 }
