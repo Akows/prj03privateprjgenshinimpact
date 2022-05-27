aa2128d23238ed4e7bb2da80f31e424f9e910e60
LeeYuSeung <akows141@gmail.com>
2022-05-27 15:35:52 +0900

    [월드정보 페이지]
    - 월드 정보 페이지 기본 구현.

--- projectfrontend/src/pages/WorldInfopage.js
+++ projectfrontend/src/pages/WorldInfopage.js
@@ -7,17 +7,14 @@ const WorldInfopage = () => {
     return (
         <>
             <div className="winf-backgroundMondstadt">
-                <a name="intromondstadt" href="#!" hidden/>
                 <IntroduceMondstadt/>
             </div>
 
             <div className="winf-backgroundLiyue">
-                <a name="introLiyue" href="#!" hidden/>
                 <IntroduceLiyue/>
             </div>
 
             <div className="winf-backgroundInazuma">
-                <a name="introInazuma" href="#!" hidden/>
                 <IntroduceInazuma/>
             </div>
         </>
