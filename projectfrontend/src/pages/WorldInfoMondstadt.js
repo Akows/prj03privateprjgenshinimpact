9a294679fb57101e72a83ed29c6699d0bc5a9bc8
LeeYuSeung <akows141@gmail.com>
2022-06-05 14:53:22 +0900

    [월드 소개 페이지 - 몬드]
    - 페이지의 기본 HTML 기반을 제작.
    - 홈페이지 HTML/CSS의 규칙성을 위한 샘플 파일 제작.

--- projectfrontend/src/pages/WorldInfoMondstadt.js
+++ projectfrontend/src/pages/WorldInfoMondstadt.js
@@ -3,10 +3,18 @@ import "../style/css/WorldInfoMondstadt.css";
 const WorldInfoMondstadt = () => {
     return (
         <>
-            <div id="WINFM-Background">
+            <div className="WINFM-BackgroundArea WINFM-BackgroundIMG1">
                 <div id='WINFM-Content-outer'>
                     <div id='WINFM-Content-ContentArea'>
+                        dd
+                    </div>
+                </div>
+            </div>
 
+            <div className="WINFM-BackgroundArea WINFM-BackgroundIMG2">
+                <div id='WINFM-Content-outer'>
+                    <div id='WINFM-Content-ContentArea'>
+                        dd
                     </div>
                 </div>
             </div>
---
+++ projectfrontend/src/sample/sectionsample.js
@@ -0,0 +1,17 @@
+import "../style/css/sectionsample.css";
+
+const sectionsample = () => {
+    return (
+        <>
+            <div className="WINFM-BackgroundArea WINFM-BackgroundIMG">
+                <div id='WINFM-Content-outer'>
+                    <div id='WINFM-Content-ContentArea'>
+                        dd
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default sectionsample;
